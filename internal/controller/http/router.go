334d70ba7da831ea43c23ca3e128577c77239718
Игорь Файзов <igorfajzov@MacBook-Pro-Igor.local>
2026-05-07 15:51:16 +0300

    update routes

--- internal/controller/http/router.go
+++ internal/controller/http/router.go
@@ -22,14 +22,16 @@ func PulseRouter(r *chi.Mux, uc *usecase.UseCase) {
 	r.Route("/api", func(r chi.Router) {
 
 		r.Route("/v1", func(r chi.Router) {
-			r.Post("/register", v1.CreateUser)
-			r.Post("/login", v1.Login)
-			r.Post("/refresh", v1.Refresh)
-			r.Post("/logout", v1.LogoutUser)
+			r.Route("/auth", func(r chi.Router) {
+				r.Post("/register", v1.CreateUser)
+				r.Post("/login", v1.Login)
+				r.Post("/refresh", v1.Refresh)
+				r.Post("/logout", v1.LogoutUser)
 
-			r.Route("/user", func(r chi.Router) {
-				r.Use(middleware.AuthMiddleware)
-				r.Get("/private", v1.Private)
+				r.Route("/user", func(r chi.Router) {
+					r.Use(middleware.AuthMiddleware)
+					r.Get("/private", v1.Private)
+				})
 			})
 		})
 	})
